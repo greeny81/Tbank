3a6b673286648431f12e162d60628b1d6d2d7386
Andy <greenyplay81@gmail.com>
2025-04-09 15:29:39 +0300

    gg wp

--- traff.py
+++ traff.py
@@ -1,17 +1,17 @@
 #[int(input('')) for x in range(3)]
 
-def inputData(elem=0,rData=''):
+def inputData(count=0,rData=''):
     anot = ['Tarif cost (q-quit)\n','Traf in tarif (q-quit)\n','cost 1mb (q-quit)\n','traf next month (q-quit)\n']
-    if(elem):
-        rnds = 4 - elem
-        count = elem
+    if(count):
+        rnds = 3 - count
         retData = rData
     else:
         count = 0
         retData = {}
-        rnds = 4
-
-    for x in range(rnds):
+        rnds = 3
+    print(f'NOW rnds {rnds}')
+    for x in range(0, rnds):# 0 Должен вычислятся!!!!
+        print(f'X:{x}')# ЮЗАТЬ Х как ключ
         try:
             ret = input(anot[count])
             if(ret == 'q'):
@@ -24,8 +24,9 @@ def inputData(elem=0,rData=''):
         else:
             count += 1
             if(len(retData) == 4):
-                print(f'ret:{retData}')
+                print(f'ret:{retData} {len(retData)}')
                 return retData
+                exit()
 
 #print(x)
 a,b,c,d = inputData().values()
